c1a5adcd846c5316d60bad22e080702122a3b0f9
beck917 <beck917@gmail.com>
2015-09-22 18:29:36 +0800

    version 6

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -5,27 +5,64 @@ public class MouseController : MonoBehaviour {
     public float jetpackForce = 75.0f;
     public float forwardMovementSpeed = 3.0f;
 
+    public Transform groundCheckTransform;
+    private bool grounded;
+    public LayerMask groundCheckLayerMask;
+    Animator animator;
+    public ParticleSystem jetpack;
+
+    private bool dead = false;
+
 	// Use this for initialization
 	void Start () {
-	
+        animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        HitByLaser(collider);
+    }
+
+    void HitByLaser(Collider2D laserCollider)
+    {
+        dead = true;
+    }
+
+    void AdjustJetpack(bool jetpackActive)
+    {
+        jetpack.enableEmission = !grounded;
+        jetpack.emissionRate = jetpackActive ? 300.0f : 75.0f;
+    }
+
+    void UpdateGroundedStatus()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+        animator.SetBool("grounded", grounded);
+    }
 
     void FixedUpdate()
     {
         bool jetpackActive = Input.GetButton("Fire1");
+        jetpackActive = jetpackActive && !dead;
 
         if (jetpackActive)
         {
             rigidbody2D.AddForce(new Vector2(0, jetpackForce));
         }
 
-        Vector2 newVelocity = rigidbody2D.velocity;
-        newVelocity.x = forwardMovementSpeed;
-        rigidbody2D.velocity = newVelocity;
+        if (dead == false)
+        {
+            Vector2 newVelocity = rigidbody2D.velocity;
+            newVelocity.x = forwardMovementSpeed;
+            rigidbody2D.velocity = newVelocity;
+        }
+
+        UpdateGroundedStatus();
+
+        AdjustJetpack(jetpackActive);
     }
 }
